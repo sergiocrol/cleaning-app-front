a898c5873640e61d8bf491c8d6c968ac23e551a6
Sergio Cordero <sergio.crol@gmail.com>
2020-06-19 11:25:10 +0200

    Update manifest and add PWA

--- src/components/signup/signup-user.component.jsx
+++ src/components/signup/signup-user.component.jsx
@@ -6,7 +6,13 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import SpinnerButton from '../spinner-button/spinner-button.component';
 
-import { UserTypeContainer, UserTypeTitle, UserTypeSubtitle, SignupForm, FormContainer } from './signup.styles';
+import {
+  UserTypeContainer,
+  UserTypeTitle,
+  UserTypeSubtitle,
+  SignupForm,
+  FormContainer,
+} from './signup.styles';
 
 const SignupUser = ({ value: { signup } }) => {
   const [isLoading, setLoading] = useState(false);
@@ -16,108 +22,124 @@ const SignupUser = ({ value: { signup } }) => {
     lastName: '',
     password: '',
     confirmPassword: '',
-    isCleaner: false
+    isCleaner: false,
   });
   const [redirect, setRedirect] = useState(null);
 
-  const { email, password, confirmPassword, firstName, lastName, isCleaner } = userCredentials;
+  const {
+    email,
+    password,
+    confirmPassword,
+    firstName,
+    lastName,
+    isCleaner,
+  } = userCredentials;
 
-  const handleInput = event => {
+  const handleInput = (event) => {
     const { value, name } = event.target;
     setCredentials({ ...userCredentials, [name]: value });
-  }
+  };
 
   // Handle form validations
   const { register, handleSubmit, errors, setError, getValues } = useForm();
 
-  const onSubmit = event => {
+  const onSubmit = (event) => {
     setLoading(true);
     signup({ email, password, firstName, lastName, isCleaner })
-      .then(res => {
+      .then((res) => {
         if (res.message) {
           setLoading(false);
           setError('email', 'alreadyExists', 'This email already exists');
           return;
-        };
+        }
         setLoading(false);
         setRedirect('/login');
       })
-      .catch(error => {
+      .catch((error) => {
         setLoading(false);
-        console.log(error)
-      })
-  }
+        console.log(error);
+      });
+  };
 
   if (redirect) {
-    return <Redirect to={redirect} />
+    return <Redirect to={redirect} />;
   }
 
   return (
     <UserTypeContainer>
-      <UserTypeTitle><Link to='/'>&#10092;</Link>Welcome to maemae</UserTypeTitle>
-      <UserTypeSubtitle>Great! Just the last step to be part of our community as user</UserTypeSubtitle>
-      <SignupForm onSubmit={handleSubmit(onSubmit)} noValidate autoComplete='off'>
+      <UserTypeTitle>
+        <Link to="/">&#10092;</Link>Welcome to maemae
+      </UserTypeTitle>
+      <UserTypeSubtitle>
+        Great! Just the last step to be part of our community as user
+      </UserTypeSubtitle>
+      <SignupForm
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
+        autoComplete="off"
+      >
         <FormContainer>
           <FormInput
-            name='firstName'
+            name="firstName"
             onChange={handleInput}
-            label='first name'
+            label="first name"
             content={firstName}
-            placeholder='first name'
+            placeholder="first name"
             register={register}
-            required='this field is required'
+            required="this field is required"
             error={errors.firstName && errors.firstName.message}
           />
           <FormInput
-            name='lastName'
+            name="lastName"
             onChange={handleInput}
-            label='last name'
+            label="last name"
             content={lastName}
-            placeholder='last name'
-            placeholderTextColor='blue'
+            placeholder="last name"
+            placeholderTextColor="blue"
             register={register}
-            required='this field is required'
+            required="this field is required"
             error={errors.lastName && errors.lastName.message}
           />
           <FormInput
-            type='email'
-            name='email'
+            type="email"
+            name="email"
             onChange={handleInput}
-            label='email'
+            label="email"
             content={email}
-            placeholder='email'
+            placeholder="email"
             register={register}
-            required='this field is required'
+            required="this field is required"
             pattern={{
               value: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
-              message: 'Must be a valid email'
+              message: 'Must be a valid email',
             }}
             error={errors.email && errors.email.message}
           />
           <FormInput
-            type='password'
-            name='password'
+            type="password"
+            name="password"
             content={password}
             onChange={handleInput}
-            label='password'
-            placeholder='password'
+            label="password"
+            placeholder="password"
             register={register}
-            required='this field is required'
+            required="this field is required"
             pattern={{
               value: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
-              message: 'Password must contain: 1 capital letter, 1 lower case letter, 1 number and 1 special character'
+              message:
+                '1 cap. letter | 1 lower case | 1 numb. | 1 sp. character',
             }}
             error={errors.password && errors.password.message}
           />
           <FormInput
-            type='password'
-            name='confirmPassword'
+            type="password"
+            name="confirmPassword"
             onChange={handleInput}
-            label='repeat password'
+            label="repeat password"
             content={confirmPassword}
-            placeholder='confirm password'
+            placeholder="confirm password"
             register={register}
-            required='this field is required'
+            required="this field is required"
             validate={{
               matchesPreviousPassword: (value) => {
                 const { password } = getValues();
@@ -127,11 +149,12 @@ const SignupUser = ({ value: { signup } }) => {
             error={errors.confirmPassword && errors.confirmPassword.message}
           />
         </FormContainer>
-        <CustomButton type='submit'>{isLoading ? <SpinnerButton /> : 'sign up'}</CustomButton>
+        <CustomButton type="submit">
+          {isLoading ? <SpinnerButton /> : 'sign up'}
+        </CustomButton>
       </SignupForm>
     </UserTypeContainer>
   );
-
-}
+};
 
 export default SignupUser;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 
@@ -18,3 +19,5 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
